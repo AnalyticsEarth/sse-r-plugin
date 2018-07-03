ee9d4ca5cbfb8eb6789cce4ee6fe02383dc9ecf0
Steven Pressland <steven.pressland@qlik.com>
2018-07-03 13:52:02 +0100

    look to remove tmp files

--- docker/packages_dist/installpackages.r
+++ docker/packages_dist/installpackages.r
@@ -1,3 +1,6 @@
-inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace")
+#inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace")
+inPack <- c("curl")
 
 install.packages(inPack, dependencies=TRUE, repos='https://cloud.r-project.org/')
+
+list.files(tempdir())
