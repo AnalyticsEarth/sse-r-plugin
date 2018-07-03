2a494dd9cc3cf449672539ccfe4c4eb48911d2cd
Steven Pressland <steven.pressland@qlik.com>
2018-07-03 13:02:29 +0100

    test with just forecast

--- docker/packages_dist/installpackages.r
+++ docker/packages_dist/installpackages.r
@@ -1,3 +1,4 @@
-inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace")
+#inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace")
+#inPack <- c(forecast")
 
 install.packages(inPack, dependencies=TRUE, repos='https://cloud.r-project.org/')
