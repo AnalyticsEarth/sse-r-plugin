8148b10761ef5d75d9e3a676d9509fb2a81d11bd
Steven Pressland <steven.pressland@qlik.com>
2018-07-03 13:05:55 +0100

    doh

--- docker/packages_dist/installpackages.r
+++ docker/packages_dist/installpackages.r
@@ -1,4 +1,4 @@
 #inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace")
-inPack <- c(forecast")
+inPack <- c("forecast")
 
 install.packages(inPack, dependencies=TRUE, repos='https://cloud.r-project.org/')
