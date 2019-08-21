234202938da3dbeece9b2c39e41c52f258d7d5fc
Steven Pressland <steven.pressland@qlik.com>
2019-08-21 13:02:14 +0100

    test

--- csharp/runservice-rserve.sh
+++ csharp/runservice-rserve.sh
@@ -15,13 +15,13 @@ fi
 # Otherwise it loops forever, waking up every 60 seconds
 
 while sleep 60; do
-  ps aux |grep Rserve |grep -q -v grep
-  PROCESS_1_STATUS=$?
+#  ps aux |grep Rserve |grep -q -v grep
+#  PROCESS_1_STATUS=$?
 
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 ]; then
-    echo "Rserve has already exited."
-    exit 1
-  fi
+#  if [ $PROCESS_1_STATUS -ne 0 ]; then
+#    echo "Rserve has already exited."
+#    exit 1
+#  fi
 done
