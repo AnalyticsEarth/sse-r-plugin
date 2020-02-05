447b99491c6d19328444eea18ddef99d74132342
Steven Pressland <steven.pressland@qlik.com>
2020-02-05 11:59:43 +0000

    Update installpackages.r

--- docker/packages_dist/installpackages.r
+++ docker/packages_dist/installpackages.r
@@ -1,3 +1,3 @@
-inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace","reshape")
+inPack <- c("jsonlite","ChainLadder","forecast","rpart","d3r","partykit","tseries","colorspace","reshape","prophet","dbscan")
 
 install.packages(inPack, dependencies=TRUE, repos='https://cloud.r-project.org/')
